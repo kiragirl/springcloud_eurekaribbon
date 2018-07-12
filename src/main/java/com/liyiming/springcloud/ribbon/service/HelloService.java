7eacb2a0feb96f000db6cf0947cd6a2c40386f40
liyiming <liyiming0215@sina.com>
2018-07-12 14:07:50 +0800

    熔断器

--- src/main/java/com/liyiming/springcloud/ribbon/service/HelloService.java
+++ src/main/java/com/liyiming/springcloud/ribbon/service/HelloService.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 /**  
  * <p>Title: HelloService</p>  
  * <p>Description: </p>  
@@ -21,8 +23,19 @@ public class HelloService {
 	
 	@Autowired
     private RestTemplate restTemplate;
-
+	/**
+	 * <p>Title: hiService</p>  
+	 * <p>Description: 熔断器 当发生熔断再请求hiService时调用 hiError</p>  
+	 * @user liyiming
+	 * @date 2018年7月12日
+	 * @param name
+	 * @return
+	 */
+	@HystrixCommand(fallbackMethod="hiError")
     public String hiService(String name) {
         return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
+    }
+	public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
     }
 }
